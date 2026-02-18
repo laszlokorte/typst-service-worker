ad0a30aa41944ee7776deb551747672ab38836d0
Laszlo Korte <email@laszlokorte.de>
2026-02-18 23:55:32 +0100

    support fonts

--- index.php
+++ index.php
@@ -4,28 +4,37 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Typst in Service worker</title>
-  <script>
-  navigator.serviceWorker.register('/worker.js', {
-    scope: '/',
-    type: "module"
-  }).then(() => {
-    document.body.appendChild(document.createTextNode("Worker registered"))
-  });
-  </script>
 </head>
 <body>
- <h1>Hello  Typst</h1>
+ <h1>Typst via Service Worker</h1>
 
+ <span id="workerStatus"></span>
+
+ <h2>Rendered as PDF</h2>
  <p>
-
+     The PDF file linked below is compiled via Service worker and then served to you.
+ </p>
+ <p>
      <a href="dynamic.pdf" target="_blank">Open PDF</a>
-
  </p>
 
- <p>
-
+ <h2>Rendered as SVG</h2>
+ <p>Below you should see the document as SVG graphic generated and served the the Service Worker<p>
+ <figure>
+     <a href="dynamic.svg" target="_blank">
      <img src="dynamic.svg" alt="SVG rendered via typst in ServiceWorker" />
- </p>
+     </a>
+ </figure>
+ <script>
+ navigator.serviceWorker.register('/worker.js', {
+   scope: '/',
+   type: "module"
+ }).then(() => {
+   window.workerStatus.appendChild(document.createTextNode("Worker registered"))
+ }).catch((e) => {
+   window.workerStatus.appendChild(document.createTextNode(e))
+ });
+ </script>
 </body>
 
 </html>
--- worker.js
+++ worker.js
@@ -1,38 +1,52 @@
 import { $typst, TypstSnippet, FetchAccessModel } from "./typst-all.js";
 
+export function preloadRemoteFonts(userFonts, options) {
+  const loader = async (_, { ref, builder }) => {
+    if (options?.fetcher) {
+      ref.setFetcher(options.fetcher);
+    }
+    await ref.loadFonts(builder, [...userFonts]);
+  };
+  loader._preloadRemoteFontOptions = options;
+  loader._kind = "fontLoader";
+  return loader;
+}
+
 self.addEventListener("install", (event) => {
+  event.waitUntil(
+    Promise.all([
+      fetch("/assets.txt").then(async (r) => {
+        const text = await r.text();
+        const files = text.split("\n").filter((f) => f.length);
+        return Promise.all(
+          files.map(async (f) => {
+            const r = await fetch(f);
+            r.arrayBuffer().then((b) => {
+              $typst.mapShadow(f, new Uint8Array(b));
+            });
+          }),
+        );
+      }),
+      fetch("/fonts.txt").then(async (fs) => {
+        const fonts = (await fs.text()).split("\n").filter((f) => f);
+
+        $typst.setCompilerInitOptions({
+          getModule: () => "./typst_ts_web_compiler_bg.wasm",
+          beforeBuild: [preloadRemoteFonts(fonts.map((f) => `./fonts/${f}`))],
+        });
+        $typst.setRendererInitOptions({
+          getModule: () => "./typst_ts_renderer_bg.wasm",
+        });
+      }),
+    ]),
+  );
   self.skipWaiting(); // optional
 });
 
 self.addEventListener("activate", (event) => {
-  $typst.setCompilerInitOptions({
-    getModule: () => "./typst_ts_web_compiler_bg.wasm",
-  });
-  $typst.setRendererInitOptions({
-    getModule: () => "./typst_ts_renderer_bg.wasm",
-  });
-
-  event.waitUntil(
-    fetch("/assets.txt").then(async (r) => {
-      const text = await r.text();
-      const files = text.split("\n").filter((f) => f.length);
-      console.log(files);
-      return Promise.all(
-        files.map(async (f) => {
-          const r = await fetch(f);
-          r.arrayBuffer().then((b) => {
-            $typst.mapShadow(f, new Uint8Array(b));
-          });
-        }),
-      );
-    }),
-  );
-
+  self.clients.claim(); // optional
   // const fetchBackend = new FetchAccessModel(import.meta.url);
   // $typst.use(TypstSnippet.withAccessModel(fetchBackend));
-
-  console.log("Initialized");
-  self.clients.claim(); // optional
 });
 
 self.addEventListener("fetch", (event) => {
@@ -40,9 +54,7 @@ self.addEventListener("fetch", (event) => {
     return;
   }
   const url = new URL(event.request.url);
-  console.log(url.pathname);
   if (url.pathname.endsWith("/dynamic")) {
-    console.log("dyn");
     const body = JSON.stringify({ time: Date.now(), foo: "bar" });
     event.respondWith(
       new Response(body, {
@@ -50,7 +62,6 @@ self.addEventListener("fetch", (event) => {
       }),
     );
   } else if (url.pathname.endsWith(".pdf")) {
-    console.log("typst");
     const newUrl = new URL(
       url.pathname.replace(/\.pdf$/, ".typst"),
       url.origin,
@@ -82,6 +93,7 @@ self.addEventListener("fetch", (event) => {
               });
             })
             .catch((e) => {
+              console.error(e);
               return new Response("Typst Error: " + e.toString(), {
                 headers: { "Content-Type": "text/plain" },
               });
@@ -93,7 +105,6 @@ self.addEventListener("fetch", (event) => {
         }),
     );
   } else if (url.pathname.endsWith(".png")) {
-    console.log("typst");
     const newUrl = new URL(url.pathname.replace(/\.png/, ".typst"), url.origin);
     event.respondWith(
       fetch(
@@ -133,7 +144,6 @@ self.addEventListener("fetch", (event) => {
         }),
     );
   } else if (url.pathname.endsWith(".svg")) {
-    console.log("typst");
     const newUrl = new URL(url.pathname.replace(/\.svg/, ".typst"), url.origin);
     event.respondWith(
       fetch(
@@ -157,9 +167,32 @@ self.addEventListener("fetch", (event) => {
           return $typst
             .svg({ mainContent: t })
             .then((pdfData) => {
-              return new Response(pdfData, {
-                headers: { "Content-Type": "image/svg+xml" },
-              });
+              return new Response(
+                pdfData.replace(
+                  /<(script|style)([^>]*)>([\s\S]*?)<\/\1>/gi,
+                  (_, tag, attrs, content) =>
+                    `<${tag}${attrs}><![CDATA[${content
+                      .replace(/document\.body/g, "document.rootElement")
+                      .replace(/"span"/g, '"text"')
+                      .replace(/addEventListener\("/g, (c) => c + "_")
+                      .replace(
+                        /createElement\(/g,
+                        (c) => "createElementNS('http://www.w3.org/2000/svg',",
+                      )
+                      .replace(
+                        'document.getElementsByTagName("head")[0]',
+                        "document.rootElement",
+                      )
+                      .replace(/\&gt;/g, ">")
+                      .replace(/\&lt;/g, "<")
+                      .replace(/\&nbsp;/g, " ")
+                      .split(";")
+                      .join(";\n")}]]></${tag}>`,
+                ),
+                {
+                  headers: { "Content-Type": "image/svg+xml" },
+                },
+              );
             })
             .catch((e) => {
               return new Response("Typst Error: " + e.toString(), {
@@ -176,3 +209,34 @@ self.addEventListener("fetch", (event) => {
     event.respondWith(fetch(event.request.clone()));
   }
 });
+
+function splitIntoChunks(str, maxLength = 100) {
+  return splitSmart(str).join("\n");
+  const chunks = [];
+
+  const regex = new RegExp(`.{1,${200}}(?:\\s+|$)`, "g");
+
+  return str
+    .match(regex)
+    .map((s) => s.trim())
+    .join("\n");
+}
+
+function splitSmart(str, maxLength = 100) {
+  const regex = /[a-zA-Z0-9\:<>/\+\-"=\.!\?]+|[^a-zA-Z0-9\:<>/\+\-"=\.!\?]+/g;
+  const parts = str.match(regex) || [];
+  const lines = [];
+  let line = "";
+
+  for (const part of parts) {
+    // if adding this part exceeds maxLength, push current line
+    if (line && line.length + part.length > maxLength) {
+      lines.push(line);
+      line = "";
+    }
+    line += part;
+  }
+
+  if (line) lines.push(line);
+  return lines;
+}
