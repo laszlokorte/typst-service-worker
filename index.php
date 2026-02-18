9d546f13e2e650848daa6121ad7051101cd91e73
Laszlo Korte <email@laszlokorte.de>
2026-02-19 00:12:57 +0100

    auto reload

--- index.php
+++ index.php
@@ -15,21 +15,23 @@
      The PDF file linked below is compiled via Service worker and then served to you.
  </p>
  <p>
-     <a href="dynamic.pdf" target="_blank">Open PDF</a>
+     <a href="docs/dynamic.pdf" target="_blank">Open PDF</a>
  </p>
 
  <h2>Rendered as SVG</h2>
  <p>Below you should see the document as SVG graphic generated and served the the Service Worker<p>
  <figure>
-     <a href="dynamic.svg" target="_blank">
-     <img src="dynamic.svg" alt="SVG rendered via typst in ServiceWorker" />
+     <a href="docs/dynamic.svg" target="_blank">
+     <img onerror="navigator.serviceWorker && setTimeout(window.location.reload.bind(window.location), 1000)" src="docs/dynamic.svg" alt="SVG rendered via typst in ServiceWorker" />
      </a>
  </figure>
  <script>
  navigator.serviceWorker.register('/worker.js', {
    scope: '/',
-   type: "module"
- }).then(() => {
+   type: "module",updateViaCache: "none",
+ }).then((s) => {
+   s.update()
+   console.log(s)
    window.workerStatus.appendChild(document.createTextNode("Worker registered"))
  }).catch((e) => {
    window.workerStatus.appendChild(document.createTextNode(e))
